cdaae2fde31c5821fdc16f6ff93b3de7b802c5fa
Andy Ye <59482767+appleband@users.noreply.github.com>
2020-09-25 13:52:54 +0800

    Added Image Drop

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {Button} from '@material-ui/core';
 import authenticationService from '../services/authenticationService';
+//port {DropzoneArea} from 'material-ui-dropzone'
+import Dropzone from 'react-dropzone'
 
 export default function HomePage(props) {
     const logout = e => {
@@ -11,7 +13,24 @@ export default function HomePage(props) {
     const createCustomer = () => {
         props.history.push("/createcustomer");
     }
+
+
+
+
+
     return(<div>
+        <Dropzone onDrop={acceptedFiles => console.log(acceptedFiles)}>
+          {({getRootProps, getInputProps}) => (
+            <section>
+            
+              <div {...getRootProps()}>
+                <input {...getInputProps()} />
+                <p>Drag 'n' drop image file here, or click to select image file</p>
+              </div>
+            </section>
+          )}
+        </Dropzone>
+
         <Button onClick={logout}>Logout</Button>
         <Button onClick={createCustomer}>Create Customer</Button>
         Im in home page!!</div>)
